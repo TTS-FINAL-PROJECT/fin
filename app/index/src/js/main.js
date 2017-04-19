7e9eafeab9fd455375a684e822b3282fea870675
Josh <josh@Joshs-MacBook-Air.local>
2017-04-18 21:20:47 -0400

    First pass at chat component

--- app/index/src/js/main.js
+++ app/index/src/js/main.js
@@ -4,15 +4,80 @@ import ReactDOM from 'react-dom'
 
 console.log('socket', socket)
 
-class App extends Component {
-  render () {
+class ChatItem extends Component {
+  render() {
+    return (<li key={this.props.index}>{this.props.author}</li>)
+  }
+}
+
+class Message extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      messages: []
+    }
+  }
+
+/*  componentDidMount () {
+    console.log("did mount")
+    this.setState({messages: this.props.messages})
+  }
+*/
+  createList () {
+    console.log("messages", this.props.messages)
+    return this.props.messages.map( (item, index) => {
+      console.log(item)
+      return (<ChatItem author={item.author} index={index}/>)
+    })
+  }
+
+  render() {
     return (
-      <div>
-        Hello From React JS!
-      </div>
+      <ul>
+        {this.createList()}
+      </ul>
     )
   }
 }
+
+
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      messages: [{
+        message: "Hello",
+        user: "Bob"
+      }]
+    }
+  }
+
+
+
+  render () {
+    return (
+      <div className="bigger-box">
+        <h1 className="person">{this.state.messages.user}</h1>
+
+        <div>
+
+          <div className="chat-area">
+            <Message messages={this.state.messages} />
+          </div>
+
+        </div>
+        <div className="controls">
+          <input className="message" type="text" placeholder="Type a message..."/>
+          <button className="send" type="submit">Send</button>
+        </div>
+      </div>
+      )
+    }
+  }
+
 
 ReactDOM.render(
   <App />,
