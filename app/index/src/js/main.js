11fedcb60e8769b151c4d133b150ed36972d9cd4
s tech <email@sarcas.tech>
2017-04-20 19:11:14 -0400

    begin implementation of sockets

--- app/index/src/js/main.js
+++ app/index/src/js/main.js
@@ -1,12 +1,27 @@
-import socket from 'socketIO'
+import Socket from 'socketIO'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-console.log('socket', socket)
+const socket = Socket()
+/*
+socket.on('user:in', function (data) {
+  console.log('user data', data)
+})
+
+socket.on('join:new', function (user) {
+  console.log('new user has joined:', user)
+})
+
+socket.emit('message:new', {author: "foo", message: "my super message"})
+
+socket.on('messages', function (msg) {
+  console.log(`client ${msg}`)
+})
+*/
 
 class ChatItem extends Component {
   render() {
-    return (<li>{this.props.author}</li>)
+    return (<li>{this.props.author}: {this.props.msg}</li>)
   }
 }
 
@@ -14,7 +29,7 @@ class Message extends Component {
 
   createList = () => (
     this.props.messages.map( (item, index) => {
-      return (<ChatItem author={item.author} key={index} />)
+      return (<ChatItem author={item.author} msg={item.message} key={index} />)
     })
   )
 
@@ -42,7 +57,23 @@ class App extends Component {
     }
   }
 
+  componentDidMount () {
+    socket.on('user:in', (data) => {
+      this.setState({currentUser: data.currentUser})
+    })
 
+    socket.on('messages', (msg) => {
+      this.setState({messages: msg})
+    })
+  }
+
+  sendMessage = (event) => {
+    event.preventDefault()
+    socket.emit('message:new', {
+      author: this.state.currentUser,
+      message: "my super message"
+    })
+  }
 
   render () {
     return (
@@ -58,7 +89,7 @@ class App extends Component {
         </div>
         <div className="controls">
           <input className="message" type="text" placeholder="Type a message..."/>
-          <button className="send" type="submit">Send</button>
+          <button className="send" onClick={this.sendMessage} type="submit">Send</button>
         </div>
       </div>
       )
